0156a4603a22525558a4b70fbf00b8530adf62fe
NoonMutaz <noonalmutaz002@gmail.com>
2026-03-06 14:05:57 +0300

    fix: id type issue

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -40,8 +40,11 @@ interface NewCustomer {
 }
 
 export default function Dashboard() {
-  const params = useParams();
-const id = Array.isArray(params.id) ? params.id[0] : params.id;
+ 
+// const id = Array.isArray(params.id) ? params.id[0] : params.id;
+
+const params = useParams();
+const id = Array.isArray(params.id) ? params.id[0] : params.id ?? "";
   // const id = params.id;
   const { projects } = useProjects();
   const { queue, addQueue, removeQueue, updateQueue,updatePriorityQueue  } = useQueueContext();
